c3e9c8dff95cb33957a936bb597fb60a08fe96fa
Maxime Mangin <maximemangin@Maximes-MBP.lan>
2024-08-03 15:40:45 +0200

    FIX: typo

--- grblHAL_Due/src/my_machine.h
+++ grblHAL_Due/src/my_machine.h
@@ -96,4 +96,4 @@
 
 /**/
 #define N_ABC_MOTORS 1
-#define DEFAULT_HOMING_CYCLE_0 (Z_AXIS_BIT|A_AXIS) // First move Z,A to clear workspace.
+#define DEFAULT_HOMING_CYCLE_0 (Z_AXIS_BIT|A_AXIS_BIT) // First move Z,A to clear workspace.
